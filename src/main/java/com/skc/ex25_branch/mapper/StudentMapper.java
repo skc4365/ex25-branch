0c88819d222d5c577cd2156bc0626fd57d983552
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-10-27 15:24:15 +0900

    eidt 매퍼

--- src/main/java/com/skc/ex25_branch/mapper/StudentMapper.java
+++ src/main/java/com/skc/ex25_branch/mapper/StudentMapper.java
@@ -2,7 +2,12 @@ package com.skc.ex25_branch.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.skc.ex25_branch.domain.Student;
 
@@ -11,11 +16,21 @@ public interface StudentMapper {
 
 	List<Student> findAll();
 
+	@Select("SELECT * FROM student WHERE id = #{id}")
 	Student findById(Long id);
 
+	
+	@Insert("INSERT INTO student(NAME, email, age) \r\n"
+			+ "  	VALUES (#{name}, #{email}, #{age})")
+	@Options(useGeneratedKeys = true, keyProperty = "id")
 	void insert(Student student);
 
+	
+	@Update("UPDATE student\r\n"
+			+ "	SET NAME = #{name}, email = #{email}, age = #{age}\r\n"
+			+ "	WHERE id = #{id}")
 	void update(Student student);
 
+	@Delete("DELETE FROM student WHERE id = #{id}")
 	void delete(Long id);
 }
