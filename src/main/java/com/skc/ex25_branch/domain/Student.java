fcebd9534d09ac59cde2f30eb20cf442868931ce
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-10-28 16:42:37 +0900

    Squashed commit of the following:
    commit 70a7a9642867db9f2a734bc2d5008b15f38f5df1
    Author: skc4365 <50658047+skc4365@users.noreply.github.com>
    Date:   Tue Oct 28 11:22:00 2025 +0900
    
        add 값검증
    
    commit e6e3acc7b061f534aea7dbb70a65f24a4ab54998
    Merge: c89ff44 206c3ef
    Author: skc4365 <50658047+skc4365@users.noreply.github.com>
    Date:   Tue Oct 28 09:36:18 2025 +0900
    
        Merge pull request #4 from skc4365/develop
    
        첫번째develop
    
    commit 206c3ef05a1e7ee7b54fb7883cb28d7334dc8b41
    Author: skc4365 <50658047+skc4365@users.noreply.github.com>
    Date:   Mon Oct 27 12:03:38 2025 +0900
    
        첫번째develop

--- src/main/java/com/skc/ex25_branch/domain/Student.java
+++ src/main/java/com/skc/ex25_branch/domain/Student.java
@@ -2,14 +2,24 @@ package com.skc.ex25_branch.domain;
 
 import java.time.LocalDateTime;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class Student {
 
 	private Long id;
+
+	@NotBlank(message = "이름은 필수항목")
 	private String name;
+	@Email(message = "이메일 형식@ 입력필수")
 	private String email;
+	@Min(value = 1, message = "나이는 1세 이상")
+
 	private Integer age;
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;
